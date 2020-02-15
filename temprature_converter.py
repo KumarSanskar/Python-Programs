bebd76319ca17a442cae13486fa0d0dc178a6907
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-02-15 11:39:40 +0530

    Converts Input in degree Celsius to Fahrenheit

---
+++ temprature_converter.py
@@ -0,0 +1,4 @@
+# takes temperature in degree celsius and converts in degree Fahrenheit
+cel = float(input('Enter temperature in  degree Celsius '))
+far = (9/5)*cel+32
+print("Temperature in degree  Fahrenheit is ", far)
