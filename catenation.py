36d564366b35652a9bd2d07f0964dd6b7ecf7fa7
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-02-15 08:44:42 +0530

    Catenation of two strings
    Concatenates two strings

---
+++ catenation.py
@@ -0,0 +1,5 @@
+# program that shows (catenation) of  two strings
+first_name = "Thor"
+last_name = "Odison"
+full_name = first_name + " " + last_name
+print(full_name)
---
+++ catenation_using_+_operand.py
@@ -0,0 +1,6 @@
+# "catenation" using + operand
+first_name = "Kumar"
+last_name = "Sanskar"
+full_name = first_name + " " + last_name
+message = "Hello," + full_name + "!"
+print(message)
