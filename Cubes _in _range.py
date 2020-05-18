3246f9f7cd93ece9ed8023232b4aec86b4cd0a09
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-05-18 14:09:04 +0530

    Returns cubes under input range.
    Returns cubes under input range.

---
+++ Cubes _in _range.py
@@ -0,0 +1,5 @@
+#  program to print cubes in an input range:
+num1 = int(input("Enter from where you want cube of number:"))
+num2 = int(input("Enter till where you want cube of number:"))
+for i in range(num1,num2):
+    print("Cube of",i,"is",i*i*i,".")
