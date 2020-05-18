a224516a17775823f094ff2ca5d31a38ad52a23d
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-05-18 14:11:27 +0530

    Using else with for loop.
    Using else with for loop.

---
+++ Using_else_with_for _loop.py
@@ -0,0 +1,7 @@
+
+#  use of else with for loop(else part gets executed only when loop is completed and there is no item left to iterate ):
+lst = [1,2,3,4,5,6,7,8,9]
+for i in lst:
+    print(i)
+else:
+    print("Items of the list have been printed.")
