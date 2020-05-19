bcfe1b29fe25cdc3901cae2e96a25a214125734a
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-05-19 19:42:09 +0530

    Using Continue statement.
    Using Continue statement

---
+++ continue_statement.py
@@ -0,0 +1,5 @@
+#  using continue statement in a program:
+for i in range(6):
+    if i == 4:
+        continue  # it simply continues after where it encountered the continue statement
+    print(i)
