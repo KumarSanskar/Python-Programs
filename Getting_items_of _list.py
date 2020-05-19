cbdf2d62242376cda7bf340182f4cfdb8707c61d
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-05-19 19:45:13 +0530

    Ways to get items of the list.
    Ways to get items of the list

---
+++ Getting_items_of _list.py
@@ -0,0 +1,21 @@
+#  Programs to access items/elements of the list:
+#  (1) Using index:- This can be used to get a specific item:
+lst = ["Ram","Ali","Hardy","Joe"]
+print("List is: ",lst)
+print("Item at second is:",lst[2])
+
+
+#  (2) Using negative index:- this can be used to acces item from backwards, (-1) represents last element:
+lst = ["Ram","Ali","Hardy","Joe"]
+print("List is: ",lst)
+print("Item at last is: ",lst[-1])
+print("Item at second last is :", lst[-2])
+
+
+# (3) Using index range:- By mentioning the index range it can be accessed:
+lst = ["Ram","Ali","Hardy","Joe","Hariom","Rebeca"]
+print("List is: ",lst)
+print("Items in range [2:5] is: ",lst[2:5])
+
+
+
