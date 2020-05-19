43678ee14adb136c626a84d8bce37d71f23e6049
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-05-19 19:45:57 +0530

    Finding length of list.
    Finding length of list

---
+++ length_of_the_list.py
@@ -0,0 +1,12 @@
+#  Program to find length of the given list:
+#  using len() function :
+lst = ["Ram","Ali","Hardy","Joe"]
+print("Length of the list is: ",len(lst))
+
+
+# using iteration i.e. a counter variable:
+lst = ["Ram","Ali","Hardy","Joe"]
+c = 0
+for i in lst:
+    c += 1
+print("Length of the list is: ",c)
