130aeaf5e51439def000caf02b03bad0f06af7e3
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-05-21 19:05:16 +0530

    Using insert in list.
    Using insert in list.

---
+++ Using_insert.py
@@ -0,0 +1,5 @@
+#  Program using ***.insert() method to add element to a list in a specified position
+Vech = ["car", "bus", "truck", "bike"]
+print("List before adding an element using insert", Vech)
+Vech.insert(2, "aeroplane")
+print("List after using insert", Vech)
