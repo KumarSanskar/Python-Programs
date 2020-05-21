9d583936f4663d041bf8220b0b45aba4d88d83d4
Kumar Sanskar <61062977+KumarSanskar@users.noreply.github.com>
2020-05-21 19:04:22 +0530

    Using append method.
    Using append method.

---
+++ Using_append.py
@@ -0,0 +1,5 @@
+#  Program using ***.append() method to add item to a list in the end:
+lst1 = ["Ram", "Shyam", "Ghanshayam"]
+print("List before using append function", lst1)
+lst1.append("Tularam")
+print("List after using append function", lst1)
